c9b206edf060a808d6046be7b52d6df862650e2f
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-11-09 02:49:22 +0900

    test($test) : 주문한 상품 만큼의 수량 존재하는지 확인하는 기능 테스트

--- src/main/java/store/ErrorType.java
+++ src/main/java/store/ErrorType.java
@@ -2,7 +2,8 @@ package store;
 
 public enum ErrorType {
     INCORRECT_PROMOTION_ERROR("[Error] 프로모션 정보가 일치하지 않습니다."),
-    NO_PRODUCT_EXIST("[ERROR] [ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+    NO_PRODUCT_EXIST("[ERROR] [ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    OVER_THAN_PRODUCT_QUANTITY("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
 
     private final String errorMessage;
 
--- src/test/java/store/ProductManagerTest.java
+++ src/test/java/store/ProductManagerTest.java
@@ -12,20 +12,16 @@ public class ProductManagerTest {
     private ProductManager productManager = new ProductManager();
 
     @Test
-    public void 존재하는_상품인지_확인() {
-        HashMap<String, Integer> testOrder = new HashMap<>();
-        testOrder.put("콜라", 10);
-        testOrder.put("감자칩", 3);
-        testOrder.put("정식도시락", 1);
-        assertTrue(productManager.checkAllProductsExist(testOrder).containsKey("콜라"), "'콜라' 상품이 존재해야 합니다.");
-        assertTrue(productManager.checkAllProductsExist(testOrder).containsKey("감자칩"), "'감자칩' 상품이 존재해야 합니다.");
-        assertTrue(productManager.checkAllProductsExist(testOrder).containsKey("정식도시락"), "'정식도시락' 상품이 존재해야 합니다.");
-    }
-
-    @Test
     public void 존재하지_않는_상품() {
         HashMap<String, Integer> testOrder = new HashMap<>();
         testOrder.put("제로콜라", 10);
         assertThatThrownBy(() -> productManager.checkAllProductsExist(testOrder)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 더_많은_수량을_주문() {
+        HashMap<String, Integer> testOrder = new HashMap<>();
+        testOrder.put("콜라", 35);
+        assertThatThrownBy(() -> productManager.checkProductsQuantity(testOrder)).isInstanceOf(IllegalArgumentException.class);
     }
 }
