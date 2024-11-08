4ee17746404bff6a0e5c6404c489f48f49ac65b8
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-11-08 23:18:44 +0900

    feat($model) : 상품 클래스 제작

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.List;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private Boolean isPromotion = false;
+    private Promotion promotion = null;
+
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, int price, int quantity, List<String> promotionInfo) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = new Promotion(promotionInfo);
+        this.isPromotion = true;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public Boolean getIsPromotion() {
+        return this.isPromotion;
+    }
+
+    public Promotion getPromotion() {
+        return this.promotion;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isQuantityZero() {
+        return (this.quantity>0);
+    }
+}
